8187a8d396277992dc1b64aaa0c808056dda81bc
ekiira <jayoluwatosin@gmail.com>
2020-06-20 10:12:19 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-/* eslint-disable no-shadow */
 import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import axios from 'axios';
 import dotenv from 'dotenv';
 import './App.css';
-import logs from './hng_logo-min.png';
+import logs from './assets/hng_logo-min.png';
+import edit from './assets/edit-alt-regular-24.png'
+import del from './assets/trash-regular-24.png'
+import check from './assets/check-regular-24.png'
+import cancel from './assets/x-regular-24.png'
 
 dotenv.config();
 
 const publics = process.env.REACT_APP_API_PUBLIC_KEYS;
 const secret = process.env.REACT_APP_API_SECOND_KEYS;
-
-
 
 Modal.setAppElement('#root');
 
@@ -26,8 +27,9 @@ const App = () => {
   const [success, setSucces] = useState(false);
   const [error, setError] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
-  const checks = '';
-  const id = '';
+  const [isEditOpen, setIsEditOpen] = useState(false);
+  const [editedAmount, setEditedAmount] = useState('')
+  
 
   // Data required for the backend
   const data1 = {
@@ -43,8 +45,17 @@ const App = () => {
     setFulldata(fullData);
   };
 
+  const openEditModal = (a) => {
+    setIsEditOpen(true);
+    setEditedAmount(a)
+  };
+
   const closeModal = () => {
     setIsOpen(false);
+  };
+
+  const closeEditModal = () => {
+    setIsEditOpen(false);
   };
 
   const onChangeHandler = (event, handler) => {
@@ -57,10 +68,21 @@ const App = () => {
   const onFormSubmit = () => { 
    axios.post('https://hng-airtime-dev-server.herokuapp.com/addIntern', data1)
       .then((res) => (res))
-      .catch((err) => {
-        console.log(err,'no');
-      });
+      .catch((err) => err);
+      setAmount('')
+      setName('')
+      setNumber('')
+      setTrack('')
   };
+
+  const onFormEditSubmit = () => { 
+    axios.post(`http://localhost:5000/edit/intern/${editedAmount}`, data1)
+       .then((res) => (res))
+       .catch((err) => {
+         console.log(err,'no');
+       });
+       setAmount('')
+   };
 
   // get interns
   useEffect(() => {
@@ -73,7 +95,7 @@ const App = () => {
       .catch((err) => {
         console.log('err', err);
       });
-  }, []);
+  }, [fullData]);
 
   const onSendAirtime = (b, c, d) => {
       const data2 = {
@@ -83,7 +105,7 @@ const App = () => {
         SecretKey: secret,
         bearer: `Bearer ${publics}`
     };
-  
+    
     axios.post('https://hng-airtime-dev-server.herokuapp.com/wallet-api', data2, {
       headers: {
         'Content-Type': 'application/json',
@@ -92,14 +114,9 @@ const App = () => {
     })
       .then((res) => {
         const { data } = res;
-        console.log(res.data);
-        setSucces(true);
         alert(data.message)
       })
-      .catch((err) => {
-        console.log(err);
-        setError(true);
-      });
+      .catch((err) => err);
   }; 
 
   // const onSendBulk = (b, c, d) => {
@@ -133,17 +150,10 @@ const App = () => {
   //   }
   // };
 
-  const onDelete = () => {
-    for (let i = 0; i < checks.length; i += 1) {
-      axios.delete(`https://hng-airtime-dev-server.herokuapp.com/removeInterns/${id[i]}`)
-        .then((res) => {
-          const { data } = res;
-          console.log(data);
-        })
-        .catch((err) => {
-          console.log('err', err);
-        });
-    }
+  const onDelete = (a) => {
+      axios.delete(`https://hng-airtime-dev-server.herokuapp.com/removeInterns/${a}`)
+        .then((res) => res)
+        .catch((err) => err);
   };
 
   const onDeleteAll = () => {
@@ -157,18 +167,6 @@ const App = () => {
       });
   };
 
-  // const onList = (a, b, c, d) => {
-  //   const data = {
-  //     a, b, c, d
-  //   }
-  //   if(check) {
-  //     setCheck(false)
-  //   } else if (!check){
-  //     setCheck(true)
-  //     // setChecked([...checked, data])
-  //     console.log(a, b, c, d, 'aa')
-  //   }
-  // }
 
   return (
     <div className="App">
@@ -180,14 +178,9 @@ const App = () => {
         </div>
       </nav>
       <h2 className="pt-5">Purchasing of Airtime Made Easy</h2>
-    {success ? <p className='text-center'>You have successfully sent airtime</p> : null}
-    {error ? <p className='text-center'>You do not have enough balance for this transaction</p> : null}
-
-      <div id="button" className='row my-3 container'>
-        <button type="button" className="btn col-6 col-md-3 button my-2 my-lg-5" onClick={openModal}>Add Intern</button>
-        <button type="button" className="btn col-6 col-md-3 button my-2 my-lg-5" onClick={onDelete}>Remove Interns</button>
-        <button type="button" className="btn col-6 col-md-3 button my-2 my-lg-5" onClick={onDeleteAll}>Remove All</button>
-        <button type="button" className="btn col-6 col-md-3 button my-2 my-lg-5">Send Bulk</button>
+      <div id="button" className=' row my-3'>
+        <button type="button" className="btn btn-block col-12 col-md-3 button mx-auto my-2 my-lg-5" onClick={openModal}>Add Intern</button>
+        <button type="button" className="btn btn-block col-12 col-md-3 button mx-auto my-2 my-lg-5" onClick={onDeleteAll}>Remove All</button>
       </div>
 
       <Modal
@@ -198,47 +191,82 @@ const App = () => {
         overlayClassName="Overlay"
       >
         <form onSubmit={onFormSubmit}>
+        <button type="button" onClick={closeModal} className="button text-center mb-3"><img src={cancel} alt='close' className='del' /></button>
           <div className="form-group">
-            <input type="text" className="form-control" value={name} onChange={(e) => onChangeHandler(e, setName)} placeholder="Intern's Name" />
+            <input type="text" className="form-control" value={name} onChange={(e) => onChangeHandler(e, setName)} placeholder="Intern's Name" required />
           </div>
           <div className="form-group">
-            <input type="text" className="form-control" value={track} onChange={(e) => onChangeHandler(e, setTrack)} placeholder="Intern's Track" />
+            <input type="text" className="form-control" value={track} onChange={(e) => onChangeHandler(e, setTrack)} required placeholder="Intern's Track" />
           </div>
           <div className="form-group">
-            <input type="text" className="form-control" value={provider} onChange={(e) => onChangeHandler(e, setProvider)} placeholder="Network Provider" />
+            <input type="text" className="form-control" value={provider} onChange={(e) => onChangeHandler(e, setProvider)} required placeholder="Network Provider" />
           </div>
           <div className="form-group">
-            <input type="text" className="form-control" value={number} onChange={(e) => onChangeHandler(e, setNumber)} placeholder="Phone Number" />
+            <input type="text" className="form-control" value={number} onChange={(e) => onChangeHandler(e, setNumber)} required placeholder="Phone Number" />
           </div>
           <div className="form-group">
-            <input type="text" className="form-control" value={amount} onChange={(e) => onChangeHandler(e, setAmount)} placeholder="Amount in naira" />
+            <input type="text" className="form-control" value={amount} onChange={(e) => onChangeHandler(e, setAmount)} required placeholder="Amount in naira" />
           </div>
           <button type="button" onClick={onFormSubmit} className="btn btn-block button text-center">Save</button>
         </form>
 
-        <button type="button" onClick={closeModal} className="btn btn-block button text-center my-3">Close</button>
 
       </Modal>
 
+      <Modal
+        isOpen={isEditOpen}
+        onRequestClose={closeModal}
+        contentLabel="Intern Amount Modal"
+        className="EditModal"
+        overlayClassName="Overlay"
+      >
+        <form onSubmit={onFormEditSubmit}>
+        <button type="button" onClick={closeEditModal} className="button text-center mr-auto mb-3"><img src={cancel} alt='close' className='del' /></button>
+
+          <div className="form-group">
+            <input type="text" className="form-control" value={amount} onChange={(e) => onChangeHandler(e, setAmount)} required placeholder="Amount in naira" />
+          </div>
+          <button type="button" onClick={onFormEditSubmit} className="btn btn-block button text-center">Save</button>
+        </form>
+
+      </Modal>
 
   <table className="table">
   <thead>
     <tr>
-      <th scope="col">Select</th>
       <th scope="col">Name</th>
       <th scope="col">Track</th>
       <th scope="col">Phone No</th>
-      <th scope="col">Action</th>
+      <th scope="col">Amount</th>
+      <th scope="col">Send</th>
+      <th scope="col">Delete</th>
+
     </tr>
   </thead>
   <tbody>
   {fullData.map(({name, number, provider, amount, _id, track}) => (
-    <tr key={_id}>
-    <td><input type="checkbox" autoComplete="off"/></td>
+    <tr key={_id}>  
     <td>{name}</td>
     <td>{track}</td>
     <td>{number}</td>
-    <td> <button type="button" className="btn button" onClick={() => onSendAirtime(number, provider, amount)}>Send Airtime</button></td>
+    <td>{amount} <img src={edit} alt='Edit' onClick={() => openEditModal(_id)} className='edit'/></td>
+    <td> 
+      <button type="button" className="button" onClick={() => onSendAirtime(number, provider, amount)}>
+      <img src={check} alt='check' className='del' />
+
+
+      </button>
+    
+      </td>
+      <td> 
+      <button type="button" className="button"  onClick={() => onDelete(_id)}>
+      <img src={del} alt='del' className='del' />
+
+
+      </button>
+
+        
+      </td>
 
     </tr>
     ))}
@@ -250,3 +278,6 @@ const App = () => {
 };
 
 export default App;
+
+{/* <button type="button" className="btn btn-block col-12 col-md-3 button mx-auto my-2 my-lg-5">Send Bulk</button> */}
+
